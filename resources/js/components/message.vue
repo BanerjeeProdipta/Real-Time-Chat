294cf829b160c534b3835050e12a5397eda6ea41
probanerjee <prodiptaj@live.com>
2020-08-25 19:23:36 +0600

    Event - Listener

---
+++ resources/js/components/message.vue
@@ -0,0 +1,19 @@
+<template>
+    <li class="list-group-item" :class="className"><slot></slot></li>
+</template>
+
+<script>
+    export default {
+        props: [
+            'color'
+        ],
+        computed: {
+            className() {
+                return 'list-group-item-'+this.color;
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,15 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css')}} ">
-    <title>Document</title>
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height:calc(100vh - 100px);
+        }
+    </style>
+    <title>Messenger</title>
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <h1>Chat</h1>
+        <div class="flex"></div>
+        <div class="flex border border-gray-300 rounded-lg" id="app">
+            <li class="list-group-item active">Private Chat</li>
+            <ul class="list-group" v-chat-scroll>
+               <message 
+               v-for="value in chat.message"
+               :key=value.index
+               color=info>
+                   @{{value}}
+               </message>
+              </ul>
+            <input type="text" class="form-control" placeholder="Typle a message..." v-model='message' @keyup.enter='send' autofocus>
         </div>
+        <div class="flex"></div>
     </div>
-    <script type="text/javascript" href="{{asset('js/app.js')}} "></script>
+    <script src="{{ asset('/js/app.js') }}"></script>
 </body>
 </html>
