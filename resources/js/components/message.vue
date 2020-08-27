5690e83d512dc501ab7667e3a4519dd3bce7bdec
probanerjee <prodiptaj@live.com>
2020-08-27 14:22:13 +0600

    avatar

--- resources/js/components/message.vue
+++ resources/js/components/message.vue
@@ -1,12 +1,25 @@
 <template>
-
 <div>
-    <div v-if="user === 'me'" class="d-flex flex-row-reverse">
-        <li class="list-group-item" :class="className"><slot></slot><small class="text-muted ml-2">{{time}}</small></li>
+    <div v-if="user === 'me'" class="d-flex align-items-center flex-row-reverse mr-2 mt-2 x">
+        <div class="ml-1">
+        <img
+            src="https://i.pravatar.cc/30"
+            alt=""
+            class="rounded-circle"
+        > 
+        </div>
+        <li id="text" class="list-group-item" :class="className"><slot></slot><small class="text-muted ml-2">{{time}}</small></li>
     </div>
 
-    <div v-if="user !== 'me'" class="d-flex flex-row">
-        <li class="list-group-item"><slot></slot><small class="text-muted ml-2">{{time}}</small></li>
+    <div v-if="user !== 'me'" class="d-flex flex-row align-items-center ml-2 mt-2">
+        <div class="mr-1">
+        <img
+            src="https://i.pravatar.cc/30"
+            alt=""
+            class="rounded-circle"
+        > 
+        </div>
+        <li id="text" class="list-group-item"><slot></slot><small class="text-muted ml-2">{{time}}</small></li>
     </div>
 
 </div>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css')}} ">
+    <link rel="stylesheet" href="{{ asset('css/chat.css')}} ">
     <style>
         .list-group{
             overflow-y: scroll;
             height:calc(100vh - 100px);
+        }
+        .text{
+            margin: .2rem;
         }
     </style>
     <title>Messenger</title>
@@ -18,19 +22,20 @@
     <div class="container">
         <div class="flex"></div>
         <div class="flex border border-gray-300 rounded-lg" id="app">
-            <li class="list-group-item active">Private Chat</li>
+            <li class="list-group-item bg-dark text-white">Private Chat</li>
+            {{-- <li class="list-group-item bg-dark text-white">Private Chat</li> --}}
             <ul class="list-group" v-chat-scroll>
                <message 
                v-for = "value,index in chat.message"
                :key = value.index
-               color = info
+               color = primary
                :user = chat.user[index]
                :time = chat.time[index]
                >
                    @{{value}}
                </message>
               </ul>
-              <div class="d-flex">@{{ typing }}</div>
+              <li class="d-flex bg-gray">@{{ typing }}</li>
             <input type="text" class="form-control" placeholder="Typle a message..." v-model='message' @keyup.enter='send' autofocus>
         </div>
         <div class="flex"></div>
